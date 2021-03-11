4f722bd528e70f942c80f313d17b4f30459cd739
smileoym <smile.oym@gmail.com>
2021-03-11 12:35:50 +0200

    added app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import BooksInput from "./components/BooksInput";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BooksInput />
+    </div>
+  );
+}
+
+export default App;
